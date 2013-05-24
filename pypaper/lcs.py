f0061a2d6a4955bb8b9dee480154619cd148f4c0
answeror <answeror@gmail.com>
2013-05-24 22:02:23 +0800

    use lcs of lit, better title search result

--- pypaper/lcs.py
+++ pypaper/lcs.py
@@ -1,19 +1,81 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def lcs(s1,s2):
-    '''http://rosettacode.org/wiki/Levenshtein_distance#Python'''
-    if len(s1) > len(s2):
-        s1,s2 = s2,s1
-    distances = range(len(s1) + 1)
-    for index2,char2 in enumerate(s2):
-        newDistances = [index2+1]
-        for index1,char1 in enumerate(s1):
-            if char1 == char2:
-                newDistances.append(distances[index1])
-            else:
-                newDistances.append(1 + min((distances[index1],
-                                             distances[index1+1],
-                                             newDistances[-1])))
-        distances = newDistances
-    return distances[-1]
+
+def lcs(a, b):
+    return levenshtein(
+        a,
+        b,
+        insertion_cost=1,
+        first_insertion_cost=50,
+        prepend_first_insertion_cost=10,
+        append_first_insertion_cost=5,
+        deletion_cost=100,
+        substitution_cost=100,
+        transposition_cost=10,
+        memo=[],
+        precol=[]
+    )
+
+
+def levenshtein(
+    s1,
+    s2,
+    deletion_cost=1,
+    insertion_cost=1,
+    first_insertion_cost=1,
+    prepend_first_insertion_cost=1,
+    append_first_insertion_cost=1,
+    substitution_cost=1,
+    transposition_cost=1,
+    memo=[],
+    precol=[]
+):
+    previous_row = 0
+    last_is_insertion = 1
+
+    if not precol:
+        a = [0 for i in range(2)]
+        a[last_is_insertion] = float('inf')
+        precol = [a] * (len(s1) + 1)
+    else:
+        assert len(precol) == len(s1) + 1
+
+    if not memo:
+        row = [[0, 0] for i in range(len(s2) + 1)]
+        row[0][last_is_insertion] = precol[0][last_is_insertion]
+        row[0][1 - last_is_insertion] = precol[0][1 - last_is_insertion]
+        for i in range(1, len(s2) + 1):
+            row[i][last_is_insertion] = min(
+                row[0][last_is_insertion],
+                row[0][1 - last_is_insertion] + (prepend_first_insertion_cost - insertion_cost)
+            ) + i * insertion_cost
+            row[i][1 - last_is_insertion] = float('inf')
+        memo.append(row)
+
+    for i, c1 in list(enumerate(s1))[len(memo) - 1:]:
+        previous_row = i
+        current_row = i + 1
+        memo.append([[0, 0] for i in range(len(s2) + 1)])
+        memo[current_row][0][1 - last_is_insertion] = (
+            min(precol[current_row])
+            + (i + 1) * deletion_cost
+        )
+        # append is not insertion
+        actual_first_insertion_cost = (
+            append_first_insertion_cost if i == len(s1) - 1
+            else first_insertion_cost
+        )
+        memo[current_row][0][last_is_insertion] = float('inf')
+        for j, c2 in enumerate(s2):
+            # j+1 instead of j since previous_row and current_row are one character longer
+            insertions = min(
+                memo[current_row][j][last_is_insertion] + insertion_cost,
+                memo[current_row][j][1 - last_is_insertion] + actual_first_insertion_cost
+            )
+            deletions = min(memo[previous_row][j + 1]) + deletion_cost
+            substitutions = min(memo[previous_row][j]) + (c1 != c2) * substitution_cost
+            memo[current_row][j + 1][last_is_insertion] = insertions
+            memo[current_row][j + 1][1 - last_is_insertion] = min(deletions, substitutions)
+
+    return min(memo[len(s1)][-1])
